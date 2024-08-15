518b5f9161c955b39abd41cc0c6972ef6d2e51ab
Guadalupe Puente Gonzalez <guadalupepuente920@gmail.com>
2024-08-15 00:32:06 -0600

    features-reservacion

--- src/app/features/user/components/reservation-form/reservation-form.component.ts
+++ src/app/features/user/components/reservation-form/reservation-form.component.ts
@@ -1,58 +1,50 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { Router } from 'express';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { take } from 'rxjs';
 import { ReservacionService } from '../../../../core/services/reservacion.service';
-import { ActivatedRoute } from '@angular/router';
-import { take} from 'rxjs';
+import { subscribe } from 'diagnostics_channel';
+import { error } from 'console';
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
-  styleUrl: './reservation-form.component.css'
+  styleUrl: './reservation-form.component.css',
+  providers: [ReservacionService]
 })
-export class ReservationFormComponent {
-  reservacionForm: FormGroup;
-  id: string | null;
+export class ReservationFormComponent implements OnInit {
   titulo = 'Hacer Reservacion';
-  constructor(
-    private fb: FormBuilder,
-    private router: Router,
-    private _reservacionService: ReservacionService,
-    private aRouter: ActivatedRoute
-  ) {
+  reservacionForm: FormGroup;
+  constructor(private fb: FormBuilder,
+    private router: Router, private _reservacionService: ReservacionService) {
     this.reservacionForm = this.fb.group({
-      fecha_ingreso: [''],
-      fecha_termino: [''],
-      hora_llegada: [''],
-      hora_salida: [''],
-      huespedes: [''],
+      fecha_ingreso: ['', Validators.required],
+      fecha_termino: ['', Validators.required],
+      hora_llegada: ['', Validators.required],
+      hora_salida: ['', Validators.required],
+      huespedes: ['', Validators.required]
     });
-    this.id = this.aRouter.snapshot.paramMap.get('id');
   }
-
-  createReservacion() {
-    const formData = new FormData();
-    formData.append('fecha_ingreso', this.reservacionForm.get('fecha_ingreso')?.value)
-    formData.append('fecha_termino', this.reservacionForm.get('fecha_termino')?.value)
-    formData.append('hora_llegada', this.reservacionForm.get('hora_llegada')?.value)
-    formData.append('hora_salida', this.reservacionForm.get('hora_salida')?.value)
-    formData.append('huespedes', this.reservacionForm.get('huespedes')?.value)
-    if ('Edit Rutina' === this.titulo) {
-      if (this.id != null) {
-        this._reservacionService.deleteReservacion(this.id).subscribe(data => {
-          alert('Reservación eliminada con exito!');
-          this.reservacionForm.reset();
-        }, error => {
-          alert(error);
-        })
-      }
-    } else {
-      this._reservacionService.createReservacion(formData).subscribe(data => {
-        alert('Reservacion agregada con exito!');
-      }, error => {
-        this.reservacionForm.reset();
-        alert(error);
-      })
+  agregarReservacion() {
+    const RESERVACION: Reservacion = {
+      fecha_ingreso: this.reservacionForm.get('fecha_ingreso')?.value,
+      fecha_termino: this.reservacionForm.get('fecha_termino')?.value,
+      hora_llegada: this.reservacionForm.get('hora_llegada')?.value,
+      hora_salida: this.reservacionForm.get('hora_salida')?.value,
+      huespedes: this.reservacionForm.get('huespedes')?.value
     }
+    console.log(RESERVACION);
+    this._reservacionService.guaradrReservacion(RESERVACION).subscribe(data => {
+      alert('Reservacion agregada con exito!');
+      this.router.navigate(['/']);
+    }, error => {
+      console.log("Ocurrió un error :/");
+      this.reservacionForm.reset();
+    });
+
+  }
+  ngOnInit(): void {
+
   }
 }
+
