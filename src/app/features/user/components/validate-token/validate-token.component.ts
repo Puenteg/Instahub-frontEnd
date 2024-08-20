d593add9fe9e964a68bc25af93e1fd06bd73f5a6
ANGELVELAZ51 <angelgerald05@outlook.com>
2024-08-20 10:02:22 -0600

    Validacion de tokens y espera a nuevas pantallas para su direccionamiento

---
+++ src/app/features/user/components/validate-token/validate-token.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../../../core/services/auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-validate-token',
+  templateUrl: './validate-token.component.html',
+  styleUrls: ['./validate-token.component.css']
+})
+export class ValidateTokenComponent {
+  tokenForm: FormGroup;
+  errorMessage: string = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.tokenForm = this.fb.group({
+      token: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.tokenForm.valid) {
+      const token = this.tokenForm.value.token;
+
+      this.authService.verificarToken(token).subscribe(
+        (res) => {
+          if (res.isValid) {
+            const rol = res.rol;
+            if (rol === 'administrador') {
+              this.router.navigate(['/admin']);
+            } else if (rol === 'anfitrion') {
+              this.router.navigate(['/anfitrion']);
+            } else if (rol === 'usuario') {
+              this.router.navigate(['/home']);
+            }
+          } else {
+            this.errorMessage = 'Token no válido. Por favor, intenta de nuevo.';
+          }
+        },
+        (error) => {
+          console.error('Error al verificar el token', error);
+          this.errorMessage = 'Ocurrió un error al verificar el token.';
+        }
+      );
+    }
+  }
+}
