3b013c311e0cdb5e499d7d21eb7179b1214c1efe
Guadalupe Puente <guadalupepuente920@gmail.com>
2024-09-05 01:30:57 -0600

    Corrección para dist en servidor

--- dist/insta-hub-front/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-R43FA4ZG.mjs";import"./chunk-DZTFQBRI.mjs";import"./chunk-DCFYTP2A.mjs";export{a as default};
--- dist/insta-hub-front/server/render-utils.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{Fb as o,Gb as r,Hb as n,Ya as e}from"./chunk-DZTFQBRI.mjs";import"./chunk-DCFYTP2A.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
